2408b10615913a5d13d5cdc11a7e880f5b9175d4
Luv <aluv49295@gmail.com>
2022-09-18 16:32:19 +0530

    SOLUTIONS

---
+++ LEETCODE SOLUTIONS/EASY/Valid_Parentheses.py
@@ -0,0 +1,20 @@
+stack = []
+for i in s:
+    if i in ['(', '{', '[']:
+        stack.append(i)
+    else:
+        if len(stack) == 0:
+            return False
+        temp = stack.pop()
+        if temp == '(' and i == ')':
+            continue
+        elif temp == '[' and i == ']':
+            continue
+        elif temp == '{' and i == '}':
+            continue
+        else:
+            return False
+if len(stack) == 0:
+    return True
+else:
+    False
