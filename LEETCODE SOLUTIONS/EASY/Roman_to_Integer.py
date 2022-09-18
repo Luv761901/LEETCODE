35902a057edf739072d9e387063a3304c0e1c6bd
Luv <aluv49295@gmail.com>
2022-09-18 15:18:53 +0530

    ROMAN TO INTEGER(LEETCODE)

---
+++ LEETCODE SOLUTIONS/EASY/Roman_to_Integer.py
@@ -0,0 +1,9 @@
+ans = 0
+d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+for i in range(len(s)-1):
+    if d[s[i]] < d[s[i+1]]:
+        ans -= d[s[i]]
+    else:
+        ans += d[s[i]]
+ans += d[s[-1]]
+return ans
