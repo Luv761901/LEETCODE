073aa7654c3754510dd1182c426f0bb6aa1750b0
Luv <aluv49295@gmail.com>
2022-09-27 09:31:02 +0530

    SOLUTIONS.py

---
+++ HARD/Contains_DuplicateIII.py
@@ -0,0 +1,11 @@
+a = []
+for i in range(len(nums)):
+    a.append([nums[i], i])
+a.sort()
+for i in range(len(a)-1):
+    j = i+1
+    while j < len(a) and abs(a[i][0]-a[j][0]) <= t:
+        if abs(a[i][1]-a[j][1]) <= k:
+            return True
+        j += 1
+return False
