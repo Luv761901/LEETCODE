b0deb05d34fd087d13f0c6aec09a25f3297b36b0
Luv <aluv49295@gmail.com>
2022-09-29 15:25:14 +0530

    SOLUTIONS.py

---
+++ EASY/Single_Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        for i in d:
+            if d[i] == 1:
+                return i
